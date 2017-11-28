610387e919f3da039863a59d199dbc741b354656
crown <chenjuncrown@gmail.com>
2017-11-28 11:17:06 +0800

    public v1.2.0

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,8 @@
         <header>
             <div class="icon"></div>
             <div class="search-box">
-                <div class="tag" @click.stop.prevent="changeSearchOptions">
-                    {{searchOptions[searchOptionsIdx].txt}}
-                </div>
-                <input type="text" id="searchInput" :placeholder="searchOptions[searchOptionsIdx].title" v-model.trim="searchStr" @keydown.tab.stop.prevent="changeSearchOptions" @keydown.up.prevent="switchUp" @keydown.down.prevent="switchDown" @keydown.enter.prevent="selectItem">
+                <div class="tag" @click.stop.prevent="changeSearchOptions">{{searchOptions[searchOptionsIdx].txt}}</div>
+                <input type="text" id="searchInput" autofocus="autofocus" :placeholder="searchOptions[searchOptionsIdx].title" v-model.trim="searchStr" @keydown.tab.stop.prevent="changeSearchOptions" @keydown.up.prevent="switchUp" @keydown.down.prevent="switchDown" @keydown.enter.prevent="selectItem">
             </div>
         </header>
         <div class="container">
@@ -42,94 +40,98 @@ export default {
             ],
             searchOptionsIdx: 0,
             checkedIdx: 0
-        };
+        }
     },
     mounted() {
-        const _this = this;
-        this.searchTabsArr = this.searchTabs();
-        chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-            _this.searchTabs();
+        const _this = this
+        this.searchTabsArr = this.searchTabs()
+        chrome.tabs.onUpdated.addListener(function() {
+            _this.searchTabs()
         })
     },
     watch: {
-        searchStr: function () {
+        searchStr: function() {
+            this.checkedIdx = 0
             switch (this.searchOptionsIdx) {
-                case 0:
-                    this.searchBookmarks()
-                    break;
-                case 1:
-                    this.searchTabs()
-                    break;
-                default:
-                    break;
+            case 0:
+                this.searchBookmarks()
+                break
+            case 1:
+                this.searchTabs()
+                break
+            default:
+                break
             }
         }
     },
     computed: {
-        searchResultsLen: function () {
-            let len = 0;
+        searchResultsLen: function() {
+            let len = 0
             switch (this.searchOptionsIdx) {
-                case 0:
-                    len = this.searchBookmarksArr.length
-                    break;
-                case 1:
-                    len = this.searchTabsArr.length
-                    break;
-                default:
-                    break;
+            case 0:
+                len = this.searchBookmarksArr.length
+                break
+            case 1:
+                len = this.searchTabsArr.length
+                break
+            default:
+                break
             }
             return len
         }
     },
     methods: {
-        searchBookmarks: debounce(function () {
-            const _this = this;
-            chrome.bookmarks.search(_this.searchStr, function (data) {
+        searchBookmarks: debounce(function() {
+            const _this = this
+            chrome.bookmarks.search(_this.searchStr, function(data) {
                 _this.searchBookmarksArr = data.filter(ele => {
                     return !ele.dateGroupModified
-                });
+                })
             })
         }, 300),
-        searchTabs: debounce(function () {
-            const _this = this;
-            chrome.tabs.query({
-                windowId: chrome.windows.WINDOW_ID_CURRENT,
-                windowType: "normal"
-            }, function (data) {
-                if (!_this.searchStr) {
-                    _this.searchTabsArr = data;
-                } else {
-                    let reg = new RegExp("(" + _this.searchStr + ")", "g")
-                    _this.searchTabsArr = data.filter(ele => {
-                        return reg.test(ele.title) || reg.test(ele.url)
-                    })
+        searchTabs: debounce(function() {
+            const _this = this
+            chrome.tabs.query(
+                {
+                    windowId: chrome.windows.WINDOW_ID_CURRENT,
+                    windowType: 'normal'
+                },
+                function(data) {
+                    if (!_this.searchStr) {
+                        _this.searchTabsArr = data
+                    } else {
+                        let reg = new RegExp('(' + _this.searchStr + ')', 'g')
+                        _this.searchTabsArr = data.filter(ele => {
+                            return reg.test(ele.title) || reg.test(ele.url)
+                        })
+                    }
                 }
-            })
+            )
         }, 300),
         switchTab(tabId) {
-            chrome.tabs.update(tabId, { highlighted: true, active: true });
+            chrome.tabs.update(tabId, { highlighted: true, active: true })
         },
-        changeSearchOptions: debounce(function () {
+        changeSearchOptions: debounce(function() {
             if (this.searchOptionsIdx + 1 < this.searchOptions.length) {
                 this.searchOptionsIdx++
             } else {
                 this.searchOptionsIdx = 0
             }
             switch (this.searchOptionsIdx) {
-                case 0:
-                    this.searchBookmarks()
-                    break;
-                case 1:
-                    this.searchTabs()
-                    break;
-                default:
-                    break;
+            case 0:
+                this.searchBookmarks()
+                break
+            case 1:
+                this.searchTabs()
+                break
+            default:
+                break
             }
-            this.checkedIdx = 0;
+            this.checkedIdx = 0
         }, 100),
         switchUp() {
             if (this.checkedIdx > 0) {
-                this.checkedIdx--;
+                this.checkedIdx--
                 if (this.checkedIdx >= 8) {
                     this.changeScrollTop()
                 }
@@ -137,7 +139,7 @@ export default {
         },
         switchDown() {
             if (this.checkedIdx < this.searchResultsLen - 1) {
-                this.checkedIdx++;
+                this.checkedIdx++
                 if (this.checkedIdx > 8) {
                     this.changeScrollTop()
                 }
@@ -147,16 +149,18 @@ export default {
             document.getElementsByClassName('item active')[0].click()
         },
         changeScrollTop() {
-            const ele = document.getElementsByClassName('container')[0];
+            const ele = document.getElementsByClassName('container')[0]
             ele.scrollTop = (this.checkedIdx - 8) * 54
         }
     },
     filters: {
         isNullUrl(url) {
-            return (url && url.indexOf('chrome-extension') === -1) ? url : './assets/icon-no.png'
+            return url && url.indexOf('chrome-extension') === -1
+                ? url
+                : './assets/icon-no.png'
         }
     }
-};
+}
 </script>
 
 <style lang="scss">

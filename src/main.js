bf8e00d72529e9924aa542f995fcccbc9dec2fff
crown <chenjuncrown@gmail.com>
2017-12-01 13:26:48 +0800

    add eslint & fix activeItem bug

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import json from './manifest.json'
-import icon from './assets/icons/crown.png'
+import json from './manifest.json' // eslint-disable-line no-unused-vars
+import icon from './assets/icons/crown.png' // eslint-disable-line no-unused-vars
 
 new Vue({
-	el: '#app',
-	render: h => h(App)
+    el: '#app',
+    render: h => h(App)
 })

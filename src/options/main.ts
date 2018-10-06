6289d806f59bc4d4cf3f42f950498f5863a72447
crown <chenjuncrown@gmail.com>
2018-10-06 09:15:02 +0800

    build($webpack): reduce webpack bundle pacjage size

--- src/options/main.ts
+++ src/options/main.ts
@@ -1,10 +1,28 @@
 import store from '@/store'
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import App from './App.vue'
 
+import VApp from 'vuetify/es5/components/VApp'
+import VGrid from 'vuetify/es5/components/VGrid'
+import VIcon from 'vuetify/es5/components/VIcon'
+import VSwitch from 'vuetify/es5/components/VSwitch'
+import VTabs from 'vuetify/es5/components/VTabs'
+import VTextField from 'vuetify/es5/components/VTextField'
+import VToolbar from 'vuetify/es5/components/VToolbar'
+import Vuetify from 'vuetify/es5/components/Vuetify'
+
 Vue.config.productionTip = false
-Vue.use(Vuetify)
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VGrid,
+    VIcon,
+    VSwitch,
+    VTabs,
+    VTextField,
+    VToolbar
+  }
+})
 
 new Vue({
   store,
--- src/popup/main.ts
+++ src/popup/main.ts
@@ -1,10 +1,25 @@
 import store from '@/store'
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import App from './App.vue'
 
+import VApp from 'vuetify/es5/components/VApp'
+import VGrid from 'vuetify/es5/components/VGrid'
+import VList from 'vuetify/es5/components/VList'
+import VTextField from 'vuetify/es5/components/VTextField'
+import VToolbar from 'vuetify/es5/components/VToolbar'
+import Vuetify from 'vuetify/es5/components/Vuetify'
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VGrid,
+    VList,
+    VTextField,
+    VToolbar
+  }
+})
+
 Vue.config.productionTip = false
-Vue.use(Vuetify)
 
 new Vue({
   store,
